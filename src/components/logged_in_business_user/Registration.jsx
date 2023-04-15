d1069d177f92b93d56c244ae52848c8d17138afc
allumellu <praveen.kumar9282@gmail.com>
2023-04-15 12:07:43 +0530

    April15 praveen

--- src/components/logged_in_business_user/Registration.jsx
+++ src/components/logged_in_business_user/Registration.jsx
@@ -8,7 +8,7 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: white;
+  background-color: #d8ddff;
 `;
 
 const RegistrationForm = styled.form`
