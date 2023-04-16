fbaad7ecfd82db4f764d72cff6e7bb08f5d46fd4
allumellu <praveen.kumar9282@gmail.com>
2023-04-16 20:54:46 +0530

    April 16 praveen

--- src/components/logged_in_business_user/Addproduct.jsx
+++ src/components/logged_in_business_user/Addproduct.jsx
@@ -18,75 +18,77 @@ const AddProduct = () => {
 
   return (
     <div className='shop-container'>
-      <h2>Add Product</h2>
-      <form onSubmit={handleSubmit} className='form'>
-        <label htmlFor='productName'>Product Name:</label>
-        <input
-          type='text'
-          id='productName'
-          value={productName}
-          onChange={(event) => setProductName(event.target.value)}
-          required
-        />
-        <label htmlFor='productCost'>Product Cost:</label>
-        <input
-          type='number'
-          id='productCost'
-          value={productCost}
-          onChange={(event) => setProductCost(event.target.value)}
-          required
-        />
-        <label htmlFor='category'>Category:</label>
-        <input
-          type='text'
-          id='category'
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-          required
-        />
-        <label htmlFor='numInStock'>Number in Stock:</label>
-        <input
-          type='number'
-          id='numInStock'
-          value={numInStock}
-          onChange={(event) => setNumInStock(event.target.value)}
-          required
-        />
-        <label htmlFor='image'>Image:</label>
-        <input
-          type='file'
-          id='image'
-          onChange={(event) => setImage(event.target.files[0])}
-          required
-        />
-        <label htmlFor='description'>Description:</label>
-        <textarea
-          id='description'
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          required
-        />
-        <label htmlFor='dimensions'>Dimensions:</label>
-        <input
-          type='text'
-          id='dimensions'
-          value={dimensions}
-          onChange={(event) => setDimensions(event.target.value)}
-          required
-        />
-        <label htmlFor='productType'>Product Type:</label>
-        <select
-          id='productType'
-          value={productType}
-          onChange={(event) => setProductType(event.target.value)}
-          required>
-          <option value=''>Select</option>
-          <option value='new'>New</option>
-          <option value='refurbished'>Refurbished</option>
-        </select>
-        <br />
-        <button type='submit'>Submit</button>
-      </form>
+      <div className='form-container'>
+        <form onSubmit={handleSubmit} className='form'>
+          <h2>Add Product</h2>
+          <label htmlFor='productName'>Product Name:</label>
+          <input
+            type='text'
+            id='productName'
+            value={productName}
+            onChange={(event) => setProductName(event.target.value)}
+            required
+          />
+          <label htmlFor='productCost'>Product Cost:</label>
+          <input
+            type='number'
+            id='productCost'
+            value={productCost}
+            onChange={(event) => setProductCost(event.target.value)}
+            required
+          />
+          <label htmlFor='category'>Category:</label>
+          <input
+            type='text'
+            id='category'
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            required
+          />
+          <label htmlFor='numInStock'>Number in Stock:</label>
+          <input
+            type='number'
+            id='numInStock'
+            value={numInStock}
+            onChange={(event) => setNumInStock(event.target.value)}
+            required
+          />
+          <label htmlFor='image'>Image:</label>
+          <input
+            type='file'
+            id='image'
+            onChange={(event) => setImage(event.target.files[0])}
+            required
+          />
+          <label htmlFor='description'>Description:</label>
+          <textarea
+            id='description'
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            required
+          />
+          <label htmlFor='dimensions'>Dimensions:</label>
+          <input
+            type='text'
+            id='dimensions'
+            value={dimensions}
+            onChange={(event) => setDimensions(event.target.value)}
+            required
+          />
+          <label htmlFor='productType'>Product Type:</label>
+          <select
+            id='productType'
+            value={productType}
+            onChange={(event) => setProductType(event.target.value)}
+            required>
+            <option value=''>Select</option>
+            <option value='new'>New</option>
+            <option value='refurbished'>Refurbished</option>
+          </select>
+          <br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
